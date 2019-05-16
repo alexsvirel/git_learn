b84d78e6c768dedffba2f018239d4cb2cc1a5a30
Alex <svirelmuz@gmail.com>
2019-05-16 22:46:11 +0300

    Update print_num.py

--- print_num.py
+++ print_num.py
@@ -1,6 +1,11 @@
+# coding=utf-8
+""" Простые действия в Python чтобы понять и научиться работать с Git и GitHub """
+
 num = 2 + 2
 print('2 + 2 = ' + str(num))
+
 num = 2 * 2
 print('2 * 2 = ' + str(num))
+
 num = 2 ** 2
 print('2 ** 2 = ' + str(num))
